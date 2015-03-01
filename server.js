ad18719ee323642814a765384855bd0fe4712dde
Soulou <leo@unbekandt.eu>
2015-03-01 15:24:40 +0100

    2.environment - dotenv

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
  * @author Léo Unbekandt
  * @version 2015/03/01
  */
+require('dotenv').load();
 
 var express = require('express')
 var app = express()
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
   res.render('index', {});
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT, function () {
   var host = server.address().address
   var port = server.address().port
   console.log('App listening at http://%s:%s', host, port)
