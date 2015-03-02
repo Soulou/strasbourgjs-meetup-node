eb221c322e9ec0d974d5a6fe60c70aaf9b0d4bf7
Soulou <leo@unbekandt.eu>
2015-03-02 18:59:44 +0100

    Bower

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require("gulp"),
     stylus = require("gulp-stylus"),
     prefixer = require("gulp-autoprefixer"),
-    shell = require("gulp-shell")
+    shell = require("gulp-shell"),
+    bower = require('main-bower-files'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    filter = require('gulp-filter');
 
 gulp.task("stylus", function() {
   return gulp.src("assets/stylesheets/*.styl")
@@ -10,7 +15,29 @@ gulp.task("stylus", function() {
     .pipe(gulp.dest('public/styles'));
 });
 
+gulp.task('bower', function() {
+  jsFilter = filter("**/*.js")
+  cssFilter = filter("**/*.css")
+  return bower()
+    .pipe(jsFilter)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('public'))
+    .pipe(jsFilter.restore())
+    .pipe(cssFilter)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('public'))
+    .pipe(cssFilter.restore())
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('minify', function() {
+  return gulp.src("public/styles/*.css")
+    .pipe(cssmin())
+    .pipe(gulp.dest("public/styles"));
+})
+
 gulp.task("watch", function() {
+  gulp.watch(['bower.json', '.bowerrc'], ['bower']);
   gulp.watch("assets/**/*.styl", ["stylus"]);
 });
 
@@ -18,4 +45,5 @@ gulp.task("supervisor", shell.task([
   "supervisor --extensions js,jade server.js"
 ]));
 
-gulp.task("dev", ["watch", "supervisor"])
+gulp.task("dev", ["bower", "watch", "supervisor"])
+gulp.task("build", ["bower", "stylus", "minify"])
